4f18d630ffe9c936a775b16359d54df8d2713af3
Christopher Baker <christopher@hmudesign.com>
2017-01-18 22:45:52 -0800

    portfolio

---
+++ public-src/routes/portfolio/portfolio.js
@@ -0,0 +1,13 @@
+import styles from './portfolio.scss';
+
+import React, { Component } from 'react';
+
+export default class Portfolio extends Component {
+	render() {
+		return (
+			<div className={ styles.main }>
+				Portfolio
+			</div>
+		);
+	}
+}
